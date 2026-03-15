1fda643d9bc788b42413b03b7fc63b0e42a3662d
Daniel Palomino Gomez <91502272+Daniel525183@users.noreply.github.com>
2026-03-15 08:01:21 +0000

    Inserted placeholder structure for the pause btn and milestone bar, adjusted grid to be 4x4, minor style changes, basic planning out of some neccesary functions

--- script.js
+++ script.js
@@ -3,12 +3,53 @@ const GOAL_CANS = 25;        // Total items needed to collect
 let currentCans = 0;         // Current number of items collected
 let gameActive = false;      // Tracks if game is currently running
 let spawnInterval;          // Holds the interval for spawning items
+const wellCost = 5;         // The score cost to free a well
 
-// Creates the 3x3 game grid where items will appear
+let wellTracker = Array.from({ length: 4 }, () => new Array(4).fill(false));
+resetWells();
+console.log(wellTracker);
+ //Will tracks which wells are free/dug
+ //False == blocked well
+ //True == free well
+
+
+
+//Function that resets wells to starting configuration
+function resetWells(){
+  console.log("TODO : resetWells")
+  if (gameActive) return;     //No resetting wells until a game isn't active.
+
+
+  //First set all wells to blocked.
+  for (let row = 0; row < 4; row++){
+    for(let col = 0; col < 4; col ++){
+      wellTracker[row][col] = 0; 
+    }
+  }
+
+  //Afterwards, set the four internal wells to be free
+  for (let row = 1; row < 3; row++){
+    for(let col = 1; col < 3; col ++){
+      wellTracker[row][col] = 1; 
+    }
+  }
+}
+
+//Function that frees a well if the player has enough score
+function digWell(){
+  console.log("TODO: digWell")
+}
+
+//Function that makes it clear that the player did not have enough points to dig a well
+function digWellFailed(){
+  console.log("TODO: digWellFailed")
+}
+
+// Creates the 4x4 game grid where items will appear
 function createGrid() {
   const grid = document.querySelector('.game-grid');
   grid.innerHTML = ''; // Clear any existing grid cells
-  for (let i = 0; i < 9; i++) {
+  for (let i = 0; i < 16; i++) {
     const cell = document.createElement('div');
     cell.className = 'grid-cell'; // Each cell represents a grid square
     grid.appendChild(cell);
@@ -40,9 +81,16 @@ function spawnWaterCan() {
 // Initializes and starts a new game
 function startGame() {
   if (gameActive) return; // Prevent starting a new game if one is already active
+  resetWells();
   gameActive = true;
   createGrid(); // Set up the game grid
+  
   spawnInterval = setInterval(spawnWaterCan, 1000); // Spawn water cans every second
+}
+
+//Function that will pause the timer and prevent any more interaction until unpaused
+function pauseGame(){
+  console.log("TODO: pauseGame()")
 }
 
 function endGame() {
